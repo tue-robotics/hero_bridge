5e34af5cc99379ade5f865919042cd4adf714fac
josja_geijsberts <j.geijsberts@student.tue.nl>
2018-08-09 13:29:41 +0200

    made result on joint trajectory bridge action server give a more reasonable result

--- src/traj_planner_node/joint_trajectory.py
+++ src/traj_planner_node/joint_trajectory.py
@@ -21,7 +21,7 @@ class JointTrajectory(object):
     def __init__(self):
 
         # server
-        self.srv_safe_joint_change = actionlib.SimpleActionServer('body/joint_trajectory_action',
+        self.srv_safe_joint_change = actionlib.SimpleActionServer('/hero/body/joint_trajectory_action',
                                                                   FollowJointTrajectoryAction,
                                                                   execute_cb=self.safe_joint_change_srv,
                                                                   auto_start=False)
@@ -36,6 +36,7 @@ class JointTrajectory(object):
         :param goal: the FollowJointTrajectoryAction type
         :return: the SafeJointChange message type
         """
+
         # helper variables
         # r = rospy.Rate(1)
         success = True
@@ -63,7 +64,10 @@ class JointTrajectory(object):
             # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
             #r.sleep()
             safeJointChange.position = point.positions
-            self.client_safe_joint_change(safeJointChange)
+            self.client_safe_joint_change.wait_for_service()
+            success=self.client_safe_joint_change(safeJointChange)
+            if not success:
+                break
              
         if success:
             rospy.loginfo('Trajectory bridge: Succeeded')
