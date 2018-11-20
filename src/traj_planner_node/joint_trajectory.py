aadd70d96faeaec48e7d117eebee5197d70c8278
Loy van Beek <loy.vanbeek@gmail.com>
2018-11-20 22:40:51 +0100

    Reduce logging and cleanup obsolute code

--- src/traj_planner_node/joint_trajectory.py
+++ src/traj_planner_node/joint_trajectory.py
@@ -21,11 +21,10 @@ sys.setdefaultencoding('utf8')
 
 class JointTrajectory(object):
     def __init__(self):
-
-        rospy.loginfo("Getting robot")
+        rospy.logdebug("Getting robot")
         self._robot = hsrb_interface.Robot()
         self._whole_body = self._robot.try_get('whole_body')
-        rospy.loginfo("Got whole_body")
+        rospy.logdebug("Got whole_body")
 
         # server
         self.srv_safe_joint_change = actionlib.SimpleActionServer('/hero/body/joint_trajectory_action',
@@ -43,129 +42,44 @@ class JointTrajectory(object):
         :param goal: the FollowJointTrajectoryAction type
         :return:
         """
-        try:
-            success = True
+        success = True
 
-            rospy.loginfo('points: {}'.format(goal.trajectory.points))
-            for j, point in enumerate(goal.trajectory.points):
-                rospy.logwarn("Process point {}: {}".format(j, point))
-                joints_goal = {}
-                for i, n in enumerate(goal.trajectory.joint_names):
-                    rospy.logwarn("Process joint {}: {}".format(i, n))
-                    joints_goal[n] = point.positions[i]
-                self.client_moveit_joint_change.wait_for_service()
-                rospy.logwarn('joints_goal is {}'.format(joints_goal))
-                try:
-                    rospy.logwarn('Call whole_body.move_to_joint_positions(joint_goal)')
-                    self._whole_body.move_to_joint_positions(joints_goal)
-                    rospy.logwarn('Call whole_body.move_to_joint_positions(joint_goal) DONE')
-                    success = True
-                except Exception as e: # TODO: catch specific exception, probably hsrb_interface.exceptions.MotionPlanningError
-                    success = False
-                    rospy.logwarn('Move_to_joint_positions raised exception "{}"'.format(e))
+        rospy.logdebug('points: {}'.format(goal.trajectory.points))
+        for j, point in enumerate(goal.trajectory.points):
+            rospy.logdebug("Process point {}: {}".format(j, point))
+            joints_goal = {}
+            for i, n in enumerate(goal.trajectory.joint_names):
+                rospy.logdebug("Process joint {}: {}".format(i, n))
+                joints_goal[n] = point.positions[i]
+            self.client_moveit_joint_change.wait_for_service()
+            rospy.logdebug('joints_goal is {}'.format(joints_goal))
+            try:
+                rospy.logdebug('Call whole_body.move_to_joint_positions(joint_goal)')
+                self._whole_body.move_to_joint_positions(joints_goal)
+                rospy.logdebug('Call whole_body.move_to_joint_positions(joint_goal) DONE')
+                success = True
+            except Exception as e: # TODO: catch specific exception, probably hsrb_interface.exceptions.MotionPlanningError
+                success = False
+                rospy.logerr('Move_to_joint_positions raised exception "{}"'.format(e))
 
-                rospy.logwarn("self.client_moveit_joint_change.wait_for_service()")
-                self.client_moveit_joint_change.wait_for_service()
-                if not success:
-                    rospy.logwarn('Could not successfully move to specified joint goal, '
-                                  'failure occurred at point({}/{}) {}'.format(j, len(goal.trajectory.points), point))
-                    # break
+            rospy.logdebug("self.client_moveit_joint_change.wait_for_service()")
+            self.client_moveit_joint_change.wait_for_service()
+            if not success:
+                rospy.logwarn('Could not successfully move to specified joint goal, '
+                              'failure occurred at point({}/{}) {}'.format(j, len(goal.trajectory.points), point))
+                # break
 
-            rospy.logwarn("All positions processed, success = {}".format(success))
+        rospy.logdebug("All positions processed, success = {}".format(success))
 
-            if success:
-                rospy.logwarn("self.srv_safe_joint_change.set_succeeded() START")
-                self.srv_safe_joint_change.set_succeeded()
-                rospy.logwarn("self.srv_safe_joint_change.set_succeeded() DONE")
-            else:
-                rospy.logwarn("self.srv_safe_joint_change.set_aborted() START")
-                self.srv_safe_joint_change.set_aborted()
-                rospy.logwarn("self.srv_safe_joint_change.set_aborted() DONE")
+        if success:
+            self.srv_safe_joint_change.set_succeeded()
+        else:
+            self.srv_safe_joint_change.set_aborted()
 
-            rospy.logwarn("moveit_joint_change_action finished")
-        except Exception as e:
-            rospy.logerr("moveit_joint_change_action raised exception: {}".format(e))
+        rospy.logdebug("moveit_joint_change_action finished")
 
 if __name__ == "__main__":
     rospy.init_node('joint_trajectory_action')
     joint_trajectory = JointTrajectory()
 
     rospy.spin()
-    
-    print("print End of spin")
-    rospy.logwarn("rospy End of spin")
-
-###########################################################
-    # def move_to_joint_positions(self, goals={}, **kwargs):
-    #     """
-    #     import math
-    #     import hsrb_interface
-    #
-    #     with hsrb_interface.Robot() as robot:
-    #        whole_body = robot.get('whole_body')
-    #        goals = {
-    #            'arm_lift_joint': 0.5,
-    #            'arm_flex_joint': math.radians(-90)
-    #        }
-    #        whole_body.move_to_joint_positions(goals)
-    #
-    #        # The method also accept keyword arguments
-    #        whole_body.move_to_joint_positions(
-    #            head_tilt_joint=math.radians(30)
-    #        )
-    #
-    #     """
-    #     if goals is None:
-    #         goals = {}
-    #     goals.update(kwargs)
-    #     if not goals:
-    #         return
-    #     goal_state = JointState()
-    #     for k, v in goals.items():
-    #         goal_state.name.append(k)
-    #         goal_state.position.append(v)
-    #     self._change_joint_state(goal_state)
-    #
-    # def _change_joint_state(self, goal_state):
-    #     """Move joints to specified joint state while checking self collision.
-    #
-    #     Args:
-    #         goal_state (sensor_msgs.msg.JointState): Target joint state
-    #     Returns:
-    #         None
-    #     Raises:
-    #         ValueError: Some specified joints are not found.
-    #         ValueError: Target joints include some uncontrollable joints.
-    #     """
-    #     # Validate joint names
-    #     initial_joint_state = self._get_joint_state()
-    #     active_joint_set = set(initial_joint_state.name)
-    #     target_joint_set = set(goal_state.name)
-    #     if not target_joint_set.issubset(active_joint_set):
-    #         unknown_set = target_joint_set.difference(active_joint_set)
-    #         msg = "No such joint(s): [{0}]".format(', '.join(unknown_set))
-    #         raise ValueError(msg)
-    #     if 'base_roll_joint' in target_joint_set:
-    #         raise ValueError(
-    #             "base_roll_joint is not supported in change_joint_state")
-    #     passive_joint_set = set(self._passive_joints)
-    #     if not target_joint_set.isdisjoint(passive_joint_set):
-    #         intersected = target_joint_set.intersection(passive_joint_set)
-    #         msg = "Passive joint(s): [{0}]".format(', '.join(intersected))
-    #         raise ValueError(msg)
-    #
-    #     req = self._generate_planning_request(PlanWithJointGoalsRequest)
-    #     goal_position = JointPosition()
-    #     goal_position.position = goal_state.position
-    #     req.use_joints = goal_state.name
-    #     req.goal_joint_states.append(goal_position)
-    #
-    #     service_name = self._setting['plan_with_joint_goals_service']
-    #     plan_service = rospy.ServiceProxy(service_name, PlanWithJointGoals)
-    #     res = plan_service.call(req)
-    #     if res.error_code.val != ArmManipulationErrorCodes.SUCCESS:
-    #         msg = "Fail to plan change_joint_state"
-    #         raise exceptions.MotionPlanningError(msg, res.error_code)
-    #     res.base_solution.header.frame_id = settings.get_frame('odom')
-    #     constrained_traj = self._constrain_trajectories(res.solution)
-    #     self._execute_trajectory(constrained_traj)
